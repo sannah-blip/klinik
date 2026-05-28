cd38d542829dc1827ba8dd8e7b0e4e2fcbe17c90
arfnnrchrs <arfnnrchrs22@gmail.com>
2026-05-28 21:34:24 +0700

    feat: konfigurasi build vite dengan tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
+import { bunny } from 'laravel-vite-plugin/fonts'; // Bisa dibiarkan atau dihapus, tidak masalah
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -8,11 +8,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
+            fonts: false, 
         }),
         tailwindcss(),
     ],
